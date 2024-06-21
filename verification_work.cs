67e31c646d4d5de1a9676f24af47e18b7a968d3a
Serzh <kserzh84@gmail.com>
2024-06-21 11:41:17 +0400

    make commit

---
+++ verification_work.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Входной массив строк
+        string[] inputArray = { "Hello", "2", "world", ":-)" };
+
+        // Создаем новый массив для хранения отфильтрованных строк
+        string[] filteredArray = new string[inputArray.Length];
+        int count = 0;
+
+        // Фильтруем строки по длине
+        foreach (var item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                filteredArray[count] = item;
+                count++;
+            }
+        }
+
+        
+
+        // Выводим результат
+        
+        Console.WriteLine("Результат:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(filteredArray[i]);
+        }
+    }
+}
+
